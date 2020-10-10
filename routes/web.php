8ff3ac03d4d7a5f3ef1bb8797072b0cc0266cf73
ahmedadil99 <ahmedadel1265@gmail.com>
2020-10-10 19:13:28 +1100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
     Route::get('/agents/{id}','App\Http\Controllers\AgentsController@view');
-    Route::get('/agents/assign-atircle/{agent_id}/{article_id}','App\Http\Controllers\AgentSController@assignArticle');
-    Route::get('/agents/view-article/{article_id}','App\Http\Controllers\AgentSController@viewTransferedArticle');
-    Route::post('/agents/update-offer/{id}','App\Http\Controllers\AgentSController@updateOffer');
+    Route::get('/agents/assign-atircle/{agent_id}/{article_id}','App\Http\Controllers\AgentsController@assignArticle');
+    Route::get('/agents/view-article/{article_id}','App\Http\Controllers\AgentsController@viewTransferedArticle');
+    Route::post('/agents/update-offer/{id}','App\Http\Controllers\AgentsController@updateOffer');
     Route::get('/agent/my-articles', 'App\Http\Controllers\AgentsController@agentsArticles');
-    Route::post('/agent/offer/{id}','App\Http\Controllers\AgentSController@agentProcessOffer');
+    Route::post('/agent/offer/{id}','App\Http\Controllers\AgentsController@agentProcessOffer');
     Route::get('/agent/view-article/{id}', 'App\Http\Controllers\AgentsController@agentArticle');
-    Route::post('/agents/send-message/{id}','App\Http\Controllers\AgentSController@sendMessage');    
+    Route::post('/agents/send-message/{id}','App\Http\Controllers\AgentsController@sendMessage');    
     Route::get('/agents','App\Http\Controllers\AgentsController@index');
     Route::post('/writer/charge/{id}', 'App\Http\Controllers\AgentsController@writerCharge');
 });
